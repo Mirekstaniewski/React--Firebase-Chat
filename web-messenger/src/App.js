fa6c3c5185527079d124c98f218ed2b1dbbbc2d2
Mirekstaniewski <mirekstaniewski@gmail.com>
2021-02-09 03:41:05 +0300

    cant fix this bags
    npm ERR! Failed at the react--firebase-chat@1.0.0 start script.
    npm ERR! This is probably not a problem with npm. There is likely additional logging output above.
    npm WARN Local package.json exists, but node_modules missing, did you mean to install?
    
    npm ERR! A complete log of this run can be found in:
    npm ERR!     C:\Users\Admin\AppData\Roaming\npm-cache\_logs\2021-02-09T00_39_07_783Z-debug.log

--- web-messenger/src/App.js
+++ web-messenger/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./containers/HomePage";
+import LoginPage from "./containers/HomePage/LoginPage";
+import RegisterPage from "./containers/HomePage/RegisterPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route path=" / " component={HomePage} />
+        <Route path=" /login " component={LoginPage} />
+        <Route path=" /signup " component={RegisterPage} />
+      </Router>
     </div>
   );
 }
--- web-messenger/src/index.js
+++ web-messenger/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSM1aQClZVURdFcvzhWsV2IZM7Zk_Xz7g",
+  authDomain: "web-messenger-e67d5.firebaseapp.com",
+  projectId: "web-messenger-e67d5",
+  storageBucket: "web-messenger-e67d5.appspot.com",
+  messagingSenderId: "659756351851",
+  appId: "1:659756351851:web:336b3732cddebdbd2d4c2e",
+  measurementId: "G-TW211TJZED"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
